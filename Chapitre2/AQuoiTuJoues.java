8af4a7dba87375d2515b9d5d529d91d803f2c8a7
BrunoDefalque <defalquebruno@gmail.com>
2022-05-22 10:15:24 +0200

    résolution des exercices chapitre de retour a la ligne et a quoi tu joues

---
+++ Chapitre2/AQuoiTuJoues.java
@@ -0,0 +1,10 @@
+/**
+ * AQuoiTuJoues
+ */
+public class AQuoiTuJoues {
+
+	public static void main(String[] args) {
+		String gameFavorite = "MarioCart";
+		System.out.println("Mon jeu favori est " + gameFavorite);
+	}
+}
---
+++ Chapitre2/RetourALaLigne.java
@@ -0,0 +1,12 @@
+/**
+ * RetourALaLigne
+ */
+public class RetourALaLigne {
+
+	public static void main(String[] args) {
+		System.out.println("Hello\nWorld");
+
+		System.out.println("Hello");
+		System.out.println("World");
+	}
+}
