89000fa81e9e04df0d4336821268a2c328df48ba
BrunoDefalque <defalquebruno@gmail.com>
2022-05-25 11:59:17 +0200

    Resol exercice Chap 2-4 BIP, ajout du fichier miseEnRoute.txt + debut exerciceChap 2-3 Scanner

---
+++ Chapitre2/Bip.java
@@ -0,0 +1,9 @@
+/**
+ * Bip
+ */
+public class Bip {
+
+	public static void main(String[] args) {
+		System.out.println("\007 \007 \007");
+	}
+}
---
+++ Chapitre2/RenvoiUneChaine.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * RenvoiUneChaine
+ */
+public class RenvoiUneChaine {
+
+	public static void main(String[] args) {
+		System.out.println();
+	}
+}
